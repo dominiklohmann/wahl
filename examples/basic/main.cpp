3b2937dee84e618e7ce0a1342645915bd391159c
Dominik Lohmann <mail@dominiklohmann.de>
2021-02-20 14:33:53 +0100

    Port functionality from pfultz2/args (#15)
    * Port pfultz2/args library
    
    * Add usage examples
    
    * Require C++17 for examples
    
    * Fix the build on MSVC
    
    * Use sudo for install only in Ubuntu CI
    
    * Fix CI check for untracked files on Windows

---
+++ examples/basic/main.cpp
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include <args/args.hpp>
+
+struct hello {
+  static const char *help() {
+    return "Simple program that greets NAME for a total of COUNT times.";
+  }
+
+  int count;
+  std::string name;
+
+  hello() : count(1) {}
+
+  template <class F> void parse(F f) {
+    f(count, "--count", "-C", args::help("Number of greetings."));
+    f(name, "--name", "-N", args::help("The person to greet."),
+      args::required());
+  }
+
+  void run() {
+    for (int i = 0; i < count; i++)
+      printf("%s\n", name.c_str());
+  }
+};
+
+int main(int argc, const char **argv) { args::parse<hello>(argc, argv); }
